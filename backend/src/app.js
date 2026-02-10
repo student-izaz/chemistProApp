f3d5181140b413558e5f871f9b327198b1481303
student izaz <izazahmed109@gmail.com>
2026-02-10 23:38:48 +0530

    connect frontend to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app"
+      "https://chemist-pro-app.vercel.app/"
     ],
     credentials: true,
   })

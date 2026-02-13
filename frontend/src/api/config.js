a0be611687c0761d386e8d94f8345bbd2ea5f6dc
student izaz <izazahmed109@gmail.com>
2026-02-13 22:24:36 +0530

    fix url issue

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,6 +1,6 @@
 const BASE_URL =
    process.env.NODE_ENV === "development"
       ? "http://localhost:5000/api"
-      : "https://chemist-pro-app.vercel.app/api";
+      : "https://chemistproapp.onrender.com/api";
 
 export default BASE_URL;
